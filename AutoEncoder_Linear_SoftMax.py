52b75a1303d76fb1ddd247366430187a83ea16ac
achreff <achref.ouni@esprit.tn>
2023-11-06 18:03:52 +0100

    Add files via upload

--- AutoEncoder_Linear_SoftMax.py
+++ AutoEncoder_Linear_SoftMax.py
@@ -1,89 +1,181 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  2 17:25:05 2023
+Created on Fri Sep 22 13:25:39 2023
 
 @author: achref
 """
+
 import torch
-from torch import nn
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5),(0.5))
-    ])
-transform = transforms.ToTensor()
-
-mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-data_loader = torch.utils.data.DataLoader(dataset=mnist_data, batch_size=64, shuffle= True)
 
 
 class Autoencoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(28*28,128),
-            nn.ReLU(),
-            nn.Linear(128,64),
-            nn.ReLU(),
-            nn.Linear(64, 12),
-            nn.ReLU(),
-            nn.Linear(12, 3)
-            )
-        
-        self.dencoder = nn.Sequential(
-            nn.Linear(3,12),
-            nn.ReLU(),
-            nn.Linear(12,64),
-            nn.ReLU(),
-            nn.Linear(64, 128),
-            nn.ReLU(),
-            nn.Linear(128,10),
     
+     def __init__(self):
+         super().__init__()
+         self.encoder = nn.Sequential(
+             nn.Linear(28*28,128),
+             nn.ReLU(),
+             nn.Linear(128,64),
+             nn.ReLU(),
+             nn.Linear(64, 12),
+             nn.ReLU(),
+             nn.Linear(12, 3)
+             )
+         
+         self.dencoder = nn.Sequential(
+             nn.Linear(3,12),
+             nn.ReLU(),
+             nn.Linear(12,64),
+             nn.ReLU(),
+             nn.Linear(64, 128),
+             nn.ReLU(),
+             nn.Linear(128,10),
+     
 
-            )
-    def forward(self,x):
-        return self.dencoder(self.encoder(x))
+             )
+     def forward(self,x):
+         return self.dencoder(self.encoder(x))
+
+def save_checkpoint(state, filename="my_checkepoint.pth.tar"):
+    print("=> Saving checkpoint")
+    torch.save(state,filename)
     
-model = Autoencoder()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr=1e-3,weight_decay=1e-5)
+def load_checkpoint(checkpoint):
+    print("=> loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    
+    
+    
+    
+device=torch.device('cude' if torch.cuda.is_available() else 'cpu')
 
-num_epochs = 10
-outputs = []
-for epoch in range(num_epochs):
-    for batch_idx, (img,targets) in enumerate(data_loader):
-        img = img.reshape(-1,28*28)
-        recon = model(img)
-        loss = criterion(recon,targets)
+num_classes=10
+lr=0.001
+bach_size= 64
+epoches= 5
+load_model=False
+
+
+train_dataset= datasets.MNIST(root='dataset/',train=True,transform=transforms.ToTensor(),download=True)
+train_loader=DataLoader(dataset=train_dataset,batch_size=bach_size,shuffle=True)
+
+
+test_dataset= datasets.MNIST(root='dataset/',train=False,transform=transforms.ToTensor(),download=True)
+test_loader=DataLoader(dataset=test_dataset,batch_size=bach_size,shuffle=True)
+
+
+model=Autoencoder().to(device)
+
+criterion=nn.CrossEntropyLoss()
+optimizer=optim.Adam(model.parameters(),lr)
+
+
+if load_model:
+    load_checkpoint(torch.load("my_checkepoint.pth.tar"))
+
+for epoch in range(epoches):
+    losses=[]
+    
+    if epoch%3 ==0:
+        checkpoint = {'state_dict' : model.state_dict(), 'optimizer': optimizer.state_dict()}
+        save_checkpoint(checkpoint)
+    
+    for batch_idx, (data,targets) in enumerate(train_loader):
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+                
+        data = data.reshape(-1,28*28)
+        #forward
+        scores = model(data)
+        loss = criterion(scores,targets)
+        losses.append(loss.item())
         
+        # backward
         optimizer.zero_grad()
         loss.backward()
-        optimizer.step()
         
-    print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
-    outputs.append((epoch,img,recon))
+        
+        #gradient descent
+        optimizer.step()
+    mean_loss= sum(losses)/len(losses)
+    print(f'Loss of ech {epoch} was {mean_loss: .5f}')
+    
+#check acc
 
+def check_acc(loader,model):
+    
+    if loader.dataset.train :
+        print('checking acc on training data')
+    else:
+        print('checking acc on test data')
+    num_correct=0
+    num_samples=0
+    model.eval()
+    with torch.no_grad():
+        for x,y in loader :
+            x=x.to(device=device)
+            y=y.to(device=device)
+            x = x.reshape(-1,28*28)
 
-for k in range(0, num_epochs, 4):
-    plt.figure(figsize=(9, 2))
-    plt.gray()
-    imgs = outputs[k][1].detach().numpy()
-    recon = outputs[k][2].detach().numpy()
-    for i, item in enumerate(imgs):
-        if i >= 9: break
-        plt.subplot(2, 9, i+1)
-        # item = item.reshape(-1, 28,28) # -> use for Autoencoder_Linear
-        # item: 1, 28, 28
-        plt.imshow(item[0])
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct+=(preds==y).sum()
+            num_samples+=preds.size(0)
             
-    for i, item in enumerate(recon):
-        if i >= 9: break
-        plt.subplot(2, 9, 9+i+1) # row_length + i + 1
-        # item = item.reshape(-1, 28,28) # -> use for Autoencoder_Linear
-        # item: 1, 28, 28
-        plt.imshow(item[0])
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100}')
+    
+    model.train()
+ 
+
+check_acc(train_loader, model)
+check_acc(test_loader, model)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
